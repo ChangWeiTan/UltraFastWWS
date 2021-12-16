21a921b6208bc795d86e39fc7eaf0db55a450e0b
changt <cwtan229@gmail.com>
2021-12-16 11:37:05 +1100

    updated run script

--- run_ultrafastwdtw.sh
+++ run_ultrafastwdtw.sh
@@ -7,4 +7,4 @@ javac -sourcepath src -cp lib/* -d bin src/experiments/TrainingTimeBenchmark.jav
 
 cd bin
 
-java -cp ../lib/*: experiments.TrainingTimeBenchmark -classifier=UltraFastWDTW -cpu=${cpu} -iter=${iter} -verbose=0 -eval=false -problem="all"
+java -cp ../lib/*: experiments.TrainingTimeBenchmark -classifier=UltraFastWDTW -cpu=${cpu} -iter=${iter} -verbose=1 -eval=false -problem="all"
