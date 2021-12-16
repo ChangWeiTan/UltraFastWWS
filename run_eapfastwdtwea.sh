6f702139c7a6010c46840e8c14466d892aef67f4
changt <cwtan229@gmail.com>
2021-12-16 11:38:30 +1100

    added EAPFastWDTWEA run script

---
+++ run_eapfastwdtwea.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cpu=8
+iter=0
+
+javac -sourcepath src -cp lib/* -d bin src/experiments/TrainingTimeBenchmark.java
+
+cd bin
+
+java -cp ../lib/*: experiments.TrainingTimeBenchmark -classifier=EAPFastWDTWEA -cpu=${cpu} -iter=${iter} -verbose=1 -eval=false -problem="all"
